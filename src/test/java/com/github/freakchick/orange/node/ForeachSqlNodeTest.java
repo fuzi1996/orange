98199ba6860a55305982e932c7b6ef06c7c8bb44
fuzi1996 <imfuzi@163.com>
2022-06-05 21:41:25 +0800

    fix:修复foreach迭代存在空语句导致分隔符多余问题

--- src/test/java/com/github/freakchick/orange/node/ForeachSqlNodeTest.java
+++ src/test/java/com/github/freakchick/orange/node/ForeachSqlNodeTest.java
@@ -19,9 +19,10 @@ import java.util.Map;
  **/
 public class ForeachSqlNodeTest {
 
+    private DynamicSqlEngine engine = new DynamicSqlEngine();
+
     @Test
     public void testEmptyIterableForeach() {
-        DynamicSqlEngine engine = new DynamicSqlEngine();
         String sql = (
                 "select * from user " +
                         "where name in " +
@@ -38,7 +39,7 @@ public class ForeachSqlNodeTest {
         map.put("list", arrayList.toArray());
         map.put("id", 100);
 
-        SqlMeta sqlMeta = engine.parse(sql, map);
+        SqlMeta sqlMeta = this.engine.parse(sql, map);
         // System.out.println(sqlMeta.getSql());
         Assert.assertEquals("select * from user where name in ",sqlMeta.getSql());
         // sqlMeta.getJdbcParamValues().forEach(System.out::println);
@@ -47,7 +48,6 @@ public class ForeachSqlNodeTest {
 
     @Test
     public void testUnEmptyIterableForeach2() {
-        DynamicSqlEngine engine = new DynamicSqlEngine();
         String sql = (
                 "select * from user " +
                         "where name in " +
@@ -68,7 +68,7 @@ public class ForeachSqlNodeTest {
         map.put("list", arrayList.toArray());
         map.put("id", 100);
 
-        SqlMeta sqlMeta = engine.parse(sql, map);
+        SqlMeta sqlMeta = this.engine.parse(sql, map);
         // System.out.println(sqlMeta.getSql());
         Assert.assertEquals("select * from user where name in  (? == ?   and id = ?)",sqlMeta.getSql());
         // sqlMeta.getJdbcParamValues().forEach(System.out::println);
@@ -77,7 +77,6 @@ public class ForeachSqlNodeTest {
 
     @Test
     public void testOrderForeach() {
-        DynamicSqlEngine engine = new DynamicSqlEngine();
         String sql = (
                 "select * from author" +
                 "<foreach item=\"item\" collection=\"orderConditions\" separator=\",\" open=\"order by\" close=\" \" index=\"index\">" +
@@ -92,9 +91,126 @@ public class ForeachSqlNodeTest {
         columns.add("id");
         map.put("orderConditions", columns);
 
-        SqlMeta sqlMeta = engine.parse(sql, map);
+        SqlMeta sqlMeta = this.engine.parse(sql, map);
         Assert.assertEquals("select * from author order by    rank,    age,    id ",sqlMeta.getSql());
         Assert.assertEquals(0,sqlMeta.getJdbcParamValues().size());
     }
 
+    @Test
+    public void shouldGetAUser(){
+        String sql = (
+                "select * from users\n" +
+                "         WHERE id in\n" +
+                "        <foreach item=\"item\" index=\"index\" collection=\"friendList\"\n" +
+                "          separator=\",\" open=\"(\" close=\")\">\n" +
+                "          ${item.id}\n" +
+                "        </foreach>"
+        );
+        Map<String, Object> map = new HashMap<>();
+        Map<String, Object> friend = new HashMap<>();
+        friend.put("id",6);
+        List<Map<String, Object>> friendList = new ArrayList<>();
+        friendList.add(friend);
+        map.put("friendList", friendList);
+
+        SqlMeta sqlMeta = this.engine.parse(sql, map);
+        //System.out.println(sqlMeta.getSql());
+        Assert.assertEquals("select * from users\n" +
+                "         WHERE id in\n" +
+                "         (\n" +
+                "          6\n" +
+                "        )",sqlMeta.getSql());
+        //sqlMeta.getJdbcParamValues().forEach(System.out::println);
+        Assert.assertEquals(0,sqlMeta.getJdbcParamValues().size());
+    }
+// https://gitee.com/freakchicken/orange/issues/I5APFR
+//    @Test
+//    public void shouldHandleComplexNullItem(){
+//        String sql = (
+//                "select count(*) from users\n" +
+//                        "      <where>\n" +
+//                        "        id in\n" +
+//                        "        <foreach item=\"item\" collection=\"list\" separator=\",\" open=\"(\" close=\")\">\n" +
+//                        "          #{item.id}\n" +
+//                        "        </foreach>\n" +
+//                        "      </where>"
+//        );
+//        Map<String, Object> map = new HashMap<>();
+//        Map<String, Object> friend = new HashMap<>();
+//        friend.put("id",6);
+//        List<Map<String, Object>> list = new ArrayList<>();
+//        list.add(friend);
+//        list.add(null);
+//        map.put("list", list);
+//
+//        SqlMeta sqlMeta = this.engine.parse(sql, map);
+//        System.out.println(sqlMeta.getSql());
+//        //Assert.assertEquals("select * from users\n",sqlMeta.getSql());
+//        sqlMeta.getJdbcParamValues().forEach(System.out::println);
+//        //Assert.assertEquals(0,sqlMeta.getJdbcParamValues().size());
+//    }
+
+//    @Test
+//    public void shouldHandleMoreComplexNullItem(){
+//        String sql = (
+//                "select count(*) from users\n" +
+//                        "      <where>\n" +
+//                        "        id in\n" +
+//                        "        <foreach item=\"item\" collection=\"list\" separator=\",\" open=\"(\" close=\")\">\n" +
+//                        "          #{item.bestFriend.id}\n" +
+//                        "        </foreach>\n" +
+//                        "      </where>"
+//        );
+//        Map<String, Object> map = new HashMap<>();
+//        Map<String, Object> bestFriend = new HashMap<>();
+//        bestFriend.put("id",5);
+//        Map<String, Object> friend = new HashMap<>();
+//        friend.put("bestFriend",bestFriend);
+//        List<Map<String, Object>> list = new ArrayList<>();
+//        list.add(friend);
+//        list.add(null);
+//        map.put("list", list);
+//
+//        SqlMeta sqlMeta = this.engine.parse(sql, map);
+//        System.out.println(sqlMeta.getSql());
+//        //Assert.assertEquals("select * from users\n",sqlMeta.getSql());
+//        sqlMeta.getJdbcParamValues().forEach(System.out::println);
+//        //Assert.assertEquals(0,sqlMeta.getJdbcParamValues().size());
+//    }
+
+    @Test
+    public void nullItemInContext(){
+        String sql = (
+                "select name from users\n" +
+                        "      <where>\n" +
+                        "        id in\n" +
+                        "        <foreach item=\"item\" collection=\"list\" separator=\",\" open=\"(\" close=\")\">\n" +
+                        "          <if test=\"item != null\">\n" +
+                        "            #{item.id}\n" +
+                        "          </if>\n" +
+                        "        </foreach>\n" +
+                        "      </where>"
+        );
+        Map<String, Object> map = new HashMap<>();
+        Map<String, Object> friend = new HashMap<>();
+        friend.put("id",3);
+        List<Map<String, Object>> list = new ArrayList<>();
+        list.add(friend);
+        list.add(null);
+        map.put("list", list);
+
+        SqlMeta sqlMeta = this.engine.parse(sql, map);
+        //System.out.println(sqlMeta.getSql());
+        Assert.assertEquals("select name from users\n" +
+                "       WHERE id in\n" +
+                "         (\n" +
+                "           \n" +
+                "            ?\n" +
+                "          \n" +
+                "        \n" +
+                "          \n" +
+                "        )",sqlMeta.getSql());
+        //sqlMeta.getJdbcParamValues().forEach(System.out::println);
+        Assert.assertEquals(1,sqlMeta.getJdbcParamValues().size());
+    }
 }
